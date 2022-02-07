fc05951962725226fedeec7e16bb271130150247
be-song <besong05@testworks.co.kr>
2022-02-07 12:16:42 +0900

    feat: add first post bambie image with next/image

--- test-app/pages/first-post.js
+++ test-app/pages/first-post.js
@@ -1,5 +1,12 @@
+import Image from 'next/image'
+
 function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <Image src="/images/bambie.png" width={400} height={500} alt="bambie" />
+    </>
+  )
 }
 
 export default FirstPost
