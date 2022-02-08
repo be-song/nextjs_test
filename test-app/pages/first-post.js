3589b8236e7c00f0a7e22e97f601bca4d4e54409
be-song <besong05@testworks.co.kr>
2022-02-08 14:26:19 +0900

    add style jsx

--- test-app/pages/first-post.js
+++ test-app/pages/first-post.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Head from 'next/head'
 
 function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First post</title>
+      </Head>
       <h1>First Post</h1>
       <Image src="/images/bambie.png" width={400} height={500} alt="bambie" />
     </>
